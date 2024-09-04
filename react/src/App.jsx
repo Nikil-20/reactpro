4cb096d7f90fa13a096f4a31b6d51345bb4e9528
nikil-20 <nikilgajankush@gmail.com>
2024-09-04 10:04:55 +0530

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,27 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import StudentSearch from "./Component/StudentSearch";
+import Navbar from "./Component/Navbar";
+import Electrical from "./Component/Electrical";
+import Civil from "./Component/Civil";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <main className="p-4">
+          <Routes>
+            <Route path="/" element={<StudentSearch />} />
+            <Route path="/electrical" element={<Electrical />} />
+            <Route path="/civil" element={<Civil />} />
+
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ react/src/Component/About.jsx
@@ -0,0 +1,27 @@
+function About(){
+    return(
+        <>
+        <h1 className="text-2xl text-center ">About Us</h1>
+        <div className="w-[100vw] flex justify-center">
+        <div className="w-[50%]">
+            <p>
+            The Hindu remains a trusted source for up-to-the-minute news, providing comprehensive coverage on the most pressing issues of our time. From the latest developments in global politics and climate change to advancements in technology and health, our mission is to keep you informed, educated, and engaged with the world around you.
+
+In an era of rapid change and constant information, our dedicated team of journalists and editors works tirelessly to bring you in-depth analysis, expert opinions, and thoroughly fact-checked reports. We understand the critical role journalism plays in shaping public discourse and empowering our readers to make informed decisions in an increasingly complex world.
+
+At The Hindu, we are unwavering in our commitment to transparency, integrity, and fairness. We uphold the highest standards of journalistic ethics, striving to earn and maintain your trust every day.
+
+Whether you’re tracking the latest global conflicts, seeking insights into economic shifts, or exploring the impact of emerging technologies, The Hindu is your go-to source for staying informed in today’s fast-paced world.
+
+Our Mission: To deliver accurate, timely, and unbiased news that not only informs but also inspires our audience to think critically and act decisively.
+
+Our Vision: To be a leading news platform that empowers individuals with the knowledge and context needed to navigate and influence a rapidly evolving world.
+
+Join Us: Stay connected with us on social media, subscribe to our newsletter, and become part of a community that values truth, transparency, and informed discussion on the most important issues of our time.
+            </p>
+        </div>
+        </div>
+        </>
+    );
+}
+export default About
